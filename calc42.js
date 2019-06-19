09572fbe8bb74a334abbf1b8b49c1f3bcecdda41
Zakery Clarke <zakerytclarke@gmail.com>
2019-06-18 20:35:52 -0600

    Minor Improvements

---
+++ calc42.js
@@ -0,0 +1,197 @@
+var stackout=[];
+
+var macros={};
+
+var varmem=[0];
+var x=0;
+while(x<999){
+varmem.length++;
+varmem[x]="0";
+var mode=0;
+
+x++;
+}
+renderstack();
+function updatecalc(i){
+	if(isNaN(i)&&i!="."){
+		if(mode==0||i=="Space"||i=="#"){
+			evaluate(i);
+		}else{
+			additem(i);
+			renderstack();
+		}
+	}else{
+		document.getElementById("input").innerHTML=document.getElementById("input").innerHTML+i;
+	}
+}
+function evaluate(i){
+	if(i=="Space"){
+		additem(parseFloat(document.getElementById("input").innerHTML));
+		document.getElementById("input").innerHTML="0";
+		renderstack();
+	}
+	if(i=="+"){
+		var arg1=getitem(0);
+		deleteitem();
+		var arg2=getitem(0);
+		deleteitem();
+		additem(arg1+arg2);
+		renderstack();
+	}
+	if(i=="-"){
+		var arg1=getitem(0);
+		deleteitem();
+		var arg2=getitem(0);
+		deleteitem();
+		additem(arg2-arg1);
+		renderstack();
+	}
+	if(i=="*"){
+		var arg1=getitem(0);
+		deleteitem();
+		var arg2=getitem(0);
+		deleteitem();
+		additem(arg2*arg1);
+		renderstack();
+	}
+	if(i=="/"){
+		var arg1=getitem(0);
+		deleteitem();
+		var arg2=getitem(0);
+		deleteitem();
+		additem(arg2/arg1);
+		renderstack();
+	}
+
+	if(i=="Rotate"){
+		var arg1=getitem(0);
+		deleteitem();
+		stackout.push(arg1);
+		renderstack();
+	}
+	if(i=="Swap"){
+		var arg1=getitem(0);
+		deleteitem();
+		var arg2=getitem(0);
+		deleteitem();
+		stackout.unshift(arg1);
+		stackout.unshift(arg2);
+		renderstack();
+	}
+	if(i=="^"){
+		var arg1=getitem(0);
+		deleteitem();
+		var arg2=getitem(0);
+		deleteitem();
+		additem(Math.pow(arg2,arg1));
+		renderstack();
+	}
+	if(i=="Del"){
+		document.getElementById("input").innerHTML="";
+		deleteitem();
+		renderstack();
+	}
+	if(i=="<"){
+		var arg1=getitem(0);
+		deleteitem();
+		var arg2=getitem(0);
+		deleteitem();
+		if(arg1>arg2){
+			additem(1);
+		}else{
+			additem(0);
+		}
+		renderstack();
+	}
+	if(i=="="){
+		var arg1=getitem(0);
+		deleteitem();
+		var arg2=getitem(0);
+		deleteitem();
+		if(arg1==arg2){
+			additem(1);
+		}else{
+			additem(0);
+		}
+		renderstack();
+	}
+	if(i==">"){
+		var arg1=getitem(0);
+		deleteitem();
+		var arg2=getitem(0);
+		deleteitem();
+		if(arg1<arg2){
+			additem(1);
+		}else{
+			additem(0);
+		}
+		renderstack();
+	}
+	if(i=="Store"){
+		var arg1=getitem(0);
+		deleteitem();
+		var arg2=getitem(0);
+		varmem[arg1]=arg2;
+		renderstack();
+	}
+	if(i=="Get"){
+		var arg1=getitem(0);
+		deleteitem();
+		additem(varmem[arg1]);
+		renderstack();
+	}
+	if(i=="Prgm"){
+		console.log(mode);
+		if(mode==0){
+			mode=1;
+			//.additem("Prgm");
+			renderstack();
+		}else{
+			mode=0;
+			var fName=prompt("Function Name");
+			var prgm=[];
+			var temp="";
+			while(temp!="Prgm"){
+				var temp=getitem(0);
+				prgm.push(temp);
+				deleteitem();
+			}
+			macros[fName]=prgm;
+		}
+
+
+	}
+
+}
+function deleteitem(){
+	var c=0;
+	while(c<stackout.length){
+		stackout[c]=stackout[c+1];
+		c++;
+	}
+	stackout.length--;
+}
+function additem(item){
+	stackout.length++;
+	var c = stackout.length-1;
+	while(c>0){
+		stackout[c]=stackout[c-1];
+		c--;
+	}
+	stackout[0]=item;
+}
+function getitem(index){
+	return stackout[index];
+}
+function renderstack(){
+	document.getElementById("output").innerHTML="";
+	var c=0;
+	var o;
+	while(c<stackout.length){
+		o="<li>";
+		o=o+stackout[c]+"</li>";
+		c++;
+		document.getElementById("output").innerHTML=document.getElementById("output").innerHTML+o;
+	}
+
+}
